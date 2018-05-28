050373e192b9635a391cca21a6df42f23c112bde
ajacks14 <ajacks14@dcs.bbk.ac.uk>
2018-05-28 18:38:58 +0100

    q4 commit

--- Q4.py
+++ Q4.py
@@ -1,26 +1,29 @@
 def truefalse(correct,id,answers):
-    correct=[]
-    answers=[]
-    for x in answers:
-        answers.append(x)
-    for x in correct:
-        correct.append(x)
-
+    answers=answers.replace(" ","")
+    correct = correct.replace(" ", "")
+    # correct2=[]
+    # answers2=[]
+    # for x in answers:
+    #     answers2.append(x)
+    # for x in correct:
+    #     correct2.append(x)
+    # print(correct2,answers2)
     Score=0
     if id == 999:
         return
     else:
         for x in range(0,len(answers)):
-            print(x)
+            print(answers[x],correct[x])
             # print(answer[x]==correct[x])
             # print(Score)
             if answers[x] == "x":
                 Score+=0
             elif answers[x] == correct[x]:
                 Score+=1
-            elif answers[x] != correct[x]:
+            # elif answers[x] != correct[x]:
+            else:
                 Score-=1
-        print(id, " ", Score, " marks")
+    print(id, " ", Score, " marks")
 
         # for x,y in zip(correct,answers):
         #     if y == 'x':
@@ -33,7 +36,7 @@ def truefalse(correct,id,answers):
 
 # print("t"=="t")
 
-truefalse("T F T F T F T F T F T F T F T F T F T F T F T F T F T F T F",100," T F T T F F T F T F T F T F T T T F F F x F T x T F T F x F")
-
+truefalse("T F T F T F T F T F T F T F T F T F T F T F T F T F T F T F",100,"T F T T F F T F T F T F T F T T T F F F x F T x T F T F x F")
+# truefalse("")
 
 
