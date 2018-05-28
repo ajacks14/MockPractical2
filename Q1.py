274b6b8d818cbdf39c51a15e6e516a7df39034d6
ajacks14 <ajacks14@dcs.bbk.ac.uk>
2018-05-28 17:52:24 +0100

    q1b commit

--- Q1.py
+++ Q1.py
@@ -1,13 +1,27 @@
-
+#q1a
 def fib(n):
     if n <=1:
         return n
     else:
         return fib(n-1) + fib(n-2)
+#
+# for x in range(20):
+#     print(fib(x),end=" ")
+#
 
-for x in range(20):
-    print(fib(x),end=" ")
 
 
+#q1b
+counter=0
+n=0
+exit=0
+while True:
+   exit=fib(n)
+   if exit >=2000:
+       break
+   print(fib(n),end=" ")
+   n+=1
+   counter+=1
 
+print("There were {} terms".format(counter))
 
